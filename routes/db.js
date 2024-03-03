5957cb436648a45b3b3d2ea8b9f5367cb7b2f38c
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-03-03 13:30:51 -0500

    finished db.js

--- routes/db.js
+++ routes/db.js
@@ -1,11 +1,56 @@
-const db = require('express').Router();
+
+const note = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const {
     readFromFile,
     readAndAppend,
-    writeTofile,
+    writeToFile,
 } = require('../helpers/fsUtils');
 
-db.get('/', (req,res) => {
+note.get('/', (req,res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-})
+});
+
+note.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const results = json.filter((note) => note.id === noteId);
+        return results.length > 0
+        ? res.json(results)
+        : res.json('no note with that ID');
+    });
+});
+
+note.delete('/:note_id', (req,res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const result = json.filter((note) => note.id !== noteId);
+
+        writeToFile('./db/db.json', result);
+
+        res,json(`Item ${noteId} has been deleted`);
+    });
+});
+
+note.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            id: uuidv4(),
+            title,
+            text
+        };
+
+        readAndAppend(newNote, ',/db/db.json');
+        res.json('Note added successfully');
+    } else {
+        res.status(400).send('Errorin adding note');
+    }
+});
+
+module.exports = note;
